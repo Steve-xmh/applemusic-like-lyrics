6b58ef878b76fa7abe6e2e49d271762afe2366e5
Steve-xmh <stevexmh@qq.com>
2023-01-29 20:52:37 +0800

    将几个 Atom 改造成 SelectAtom

--- src/core/states.ts
+++ src/core/states.ts
@@ -1,4 +1,5 @@
-import { atom } from "jotai";
+import { Atom, atom } from "jotai";
+import { selectAtom } from "jotai/utils";
 import { getPlayingSong, LyricFile, PlayState } from "../api";
 import { LyricLine } from "./lyric-parser";
 
@@ -10,26 +11,33 @@ export const playStateAtom = atom(PlayState.Pausing);
 export const currentLyricsAtom = atom<LyricLine[] | null>(null);
 export const currentLyricsIndexAtom = atom(-1);
 export const playingSongDataAtom = atom(getPlayingSong());
-export const musicIdAtom = atom<string | number>((get) => {
-	const playing = get(playingSongDataAtom);
-	return (
-		playing?.originFromTrack?.lrcid ||
-		playing?.originFromTrack?.track?.tid ||
-		playing?.data?.id ||
-		0
-	);
-});
-export const albumAtom = atom(
-	(get) => get(playingSongDataAtom)?.data?.album || {},
+
+export const musicIdAtom: Atom<string | number> = selectAtom(
+	playingSongDataAtom,
+	(playing) => {
+		return (
+			playing?.originFromTrack?.lrcid ||
+			playing?.originFromTrack?.track?.tid ||
+			playing?.data?.id ||
+			0
+		);
+	},
 );
-export const songNameAtom = atom(
-	(get) => get(playingSongDataAtom)?.data?.name || "未知歌名",
+export const albumAtom = selectAtom(
+	playingSongDataAtom,
+	(playing) => playing?.data?.album || {},
 );
-export const songAliasNameAtom = atom(
-	(get) => get(playingSongDataAtom)?.data?.alias || [],
+export const songNameAtom = selectAtom(
+	playingSongDataAtom,
+	(playing) => playing?.data?.name || "未知歌名",
 );
-export const songArtistsAtom = atom(
-	(get) => get(playingSongDataAtom)?.data?.artists || [],
+export const songAliasNameAtom = selectAtom(
+	playingSongDataAtom,
+	(playing) => playing?.data?.alias || [],
+);
+export const songArtistsAtom = selectAtom(
+	playingSongDataAtom,
+	(playing) => playing?.data?.artists || [],
 );
 export const getMusicId = (): number | string =>
 	getPlayingSong()?.originFromTrack?.lrcid ||
