a5661457428eeba0d6ceeb454443507aea76510c
Steve-xmh <stevexmh@qq.com>
2023-02-07 20:24:30 +0800

    更新到 1.8.0

--- dist/startup_script.js
+++ dist/startup_script.js
@@ -1,1 +1,1 @@
-(()=>{var l=typeof WorkerGlobalScope<"u"&&self instanceof WorkerGlobalScope;var s=()=>{};var r=s;var t=channel.call;channel.call=function(e,...n){if(e==="storage.downloadscanner")r(e,...n,new Error().stack);else return t.apply(t,[e,...n])};})();
+(()=>{var a=typeof WorkerGlobalScope<"u"&&self instanceof WorkerGlobalScope;var l=()=>{};var r=l;var t=channel.call;channel.call=function(e,...n){if(e==="storage.downloadscanner")r(e,...n,new Error().stack);else return t.apply(t,[e,...n])};})();
