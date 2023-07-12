b882bf7867cf2358c1d21a19042ef76458711da0
SteveXMH <39523898+Steve-xmh@users.noreply.github.com>
2023-07-12 14:12:53 +0800

    写了一半

--- src/components/lyric-dom-renderer/lyric-line.tsx
+++ src/components/lyric-dom-renderer/lyric-line.tsx
@@ -7,7 +7,46 @@ import { rightClickedLyricAtom } from "../../core/states";
 import * as React from "react";
 import type { DynamicLyricWord } from "../../core/lyric-types";
 import { useSpring } from "../../utils/spring-svelte";
-import { warn } from "../../utils/logger";
+import { log, warn } from "../../utils/logger";
+
+const FADE_FRAMES = 100;
+const FADE_WIDTH = 20;
+const LYRIC_WORD_ANIMATION: Keyframe[] = new Array(FADE_FRAMES + FADE_WIDTH)
+	.fill(0)
+	.map((_, i) => {
+		const maskImg = `linear-gradient(to right, #000 ${
+			i - FADE_WIDTH
+		}%, rgba(0, 0, 0, 0.5) ${i + FADE_WIDTH}%)`;
+		return {
+			webkitMaskImage: maskImg,
+			maskImage: maskImg,
+			offset: i / (FADE_FRAMES + FADE_WIDTH),
+		};
+	});
+log("LYRIC_WORD_ANIMATION", LYRIC_WORD_ANIMATION);
+const LYRIC_WORD_GLOW_ANIMATION: Keyframe[] = [
+	{
+		offset: 0,
+		transform: "translate3d(0, 0px, 0px)",
+		opacity: 0.5,
+		filter:
+			"drop-shadow(0 0 0 var(--applemusic-like-lyrics-font-color, white))",
+	},
+	{
+		offset: 0.5,
+		transform: "translate3d(0, -1px, 20px)",
+		opacity: 1,
+		filter:
+			"drop-shadow(0 0 4px var(--applemusic-like-lyrics-font-color, white))",
+	},
+	{
+		offset: 1,
+		transform: "translate3d(0, 0px, 0)",
+		opacity: 1,
+		filter:
+			"drop-shadow(0 0 0 var(--applemusic-like-lyrics-font-color, white))",
+	},
+];
 
 const LyricWord: React.FC<{
 	word: DynamicLyricWord;
@@ -20,11 +59,17 @@ const LyricWord: React.FC<{
 	const letterDuration = duration / letters.length / 2;
 	const floatDuration = Math.max(1000, word.duration);
 	const wordRef = React.useRef<HTMLSpanElement>(null);
-	const wordAnimationRef = React.useRef<Animation>();
+	const glowWordRef = React.useRef<HTMLSpanElement>(null);
+	const wordFloatAnimationRef = React.useRef<Animation>();
+	const wordMainAnimationRef = React.useRef<Animation[]>([]);
+	// 悬浮效果
 	React.useLayoutEffect(() => {
-		if (!wordAnimationRef.current && wordRef.current) {
+		if (
+			!wordFloatAnimationRef.current &&
+			(wordRef.current || glowWordRef.current)
+		) {
 			try {
-				const a = wordRef.current.animate(
+				const a = (wordRef.current ?? glowWordRef.current)?.animate(
 					[
 						{
 							transform: "translateY(0px)",
@@ -36,43 +81,121 @@ const LyricWord: React.FC<{
 					{
 						duration: floatDuration,
 						delay,
+						id: "float-word",
+						composite: "add",
 						fill: "both",
 					},
 				);
-				a.pause();
-				wordAnimationRef.current = a;
+				a?.persist();
+				a?.pause();
+				wordFloatAnimationRef.current = a;
 			} catch (err) {
 				warn("应用单词悬浮动画失败", err);
 			}
 		}
 	}, [floatDuration]);
+	// 渐变效果或发光效果
 	React.useLayoutEffect(() => {
-		if (wordRef.current && wordAnimationRef.current) {
-			if (selected) {
-				wordAnimationRef.current.play();
-			} else {
-				wordAnimationRef.current.reverse();
+		if (word.shouldGlow && glowWordRef.current) {
+			wordMainAnimationRef.current.forEach((a) => {
+				a.finish();
+				a.cancel();
+			});
+			wordMainAnimationRef.current.splice(
+				0,
+				wordMainAnimationRef.current.length,
+			);
+			let i = 0;
+			for (const letter of glowWordRef.current.children) {
+				const a = letter.animate(LYRIC_WORD_GLOW_ANIMATION, {
+					duration: duration - i * letterDuration,
+					delay: delay + i * letterDuration,
+					id: "glow-word",
+					easing: "ease-in-out",
+					composite: "add",
+					fill: "both",
+				});
+				a.persist();
+				a.pause();
+				wordMainAnimationRef.current.push(a);
+				i++;
+			}
+		} else if (wordRef.current) {
+			wordMainAnimationRef.current.forEach((a) => {
+				a.finish();
+				a.cancel();
+			});
+			wordMainAnimationRef.current.splice(
+				0,
+				wordMainAnimationRef.current.length,
+			);
+			let canceled = false;
+			const onFrame = () => {
+				if (wordRef.current) {
+					const maskImage = ``;
+					wordRef.current.style.webkitMaskImage = maskImage;
+					wordRef.current.style.maskImage = maskImage;
+					requestAnimationFrame(onFrame);
+				}
+			}
+			requestAnimationFrame(onFrame);
+			return () => {
+				canceled = true;
+				if (wordRef.current) {
+					wordRef.current.style.webkitMaskImage = "";
+					wordRef.current.style.maskImage = "";
+				}
 			}
 		}
-	}, [wordRef.current, selected, wordAnimationRef.current]);
+	}, [
+		word.duration,
+		word.shouldGlow,
+		delay,
+		letterDuration,
+		glowWordRef.current,
+		wordRef.current,
+	]);
+	// 播放动画
+	React.useLayoutEffect(() => {
+		if (wordRef.current || glowWordRef.current) {
+			if (selected) {
+				if (wordFloatAnimationRef.current) {
+					wordFloatAnimationRef.current.updatePlaybackRate(1);
+					wordFloatAnimationRef.current.play();
+				}
+				wordMainAnimationRef.current.forEach((a) => {
+					a.playbackRate = 1;
+					a.play();
+				});
+				log(
+					"播放动画",
+					wordFloatAnimationRef.current,
+					...wordMainAnimationRef.current,
+				);
+			} else {
+				if (wordFloatAnimationRef.current) {
+					wordFloatAnimationRef.current.updatePlaybackRate(-1);
+					wordFloatAnimationRef.current.play();
+				}
+				wordMainAnimationRef.current.forEach((a) => {
+					a.playbackRate = -1;
+					if (a.playState === "finished") {
+						a.play();
+					}
+				});
+				log(
+					"回滚动画",
+					wordFloatAnimationRef.current,
+					...wordMainAnimationRef.current,
+				);
+			}
+		}
+	}, [selected]);
 	if (word.shouldGlow) {
 		return (
-			<span
-				className="am-lyric-glow-word"
-				style={{
-					animationDelay: `${delay}ms`,
-					animationDuration: `${duration}ms`,
-				}}
-			>
-				{letters.map((v, i) => (
-					<span
-						style={{
-							animationDelay: `${delay + i * letterDuration}ms`,
-							animationDuration: `${duration - i * letterDuration}ms`,
-						}}
-					>
-						{v}
-					</span>
+			<span className="am-lyric-glow-word" ref={glowWordRef}>
+				{letters.map((v) => (
+					<span>{v}</span>
 				))}
 			</span>
 		);
@@ -80,10 +203,6 @@ const LyricWord: React.FC<{
 		return (
 			<span
 				key={`am-lyric-real-word dynamic-word-${word.word}-${index}`}
-				style={{
-					animationDelay: `${delay}ms`,
-					animationDuration: `${word.duration}ms`,
-				}}
 				ref={wordRef}
 			>
 				{word.word}
@@ -148,10 +267,10 @@ export const LyricLineView: React.FC<
 			};
 		}
 	}, [onSizeChanged]);
-	
+
 	React.useLayoutEffect(() => {
-		setShouldKeepDynamic(v => v || selected);
-	}, [selected])
+		setShouldKeepDynamic((v) => v || selected);
+	}, [selected]);
 
 	React.useLayoutEffect(() => {
 		if (lineRef.current) {
@@ -245,6 +364,9 @@ export const LyricLineView: React.FC<
 						lineRef.current.style.transform = `translateY(${lineTransform.top}px) translateX(${lineTransform.left}px) scale(${lineTransform.scale})`;
 					setShouldKeepDynamic(false);
 				});
+			return () => {
+				setShouldKeepDynamic(false);
+			};
 		} else {
 			const h = setTimeout(() => {
 				springRef.current
@@ -267,6 +389,7 @@ export const LyricLineView: React.FC<
 			}, lineTransform.delay);
 			return () => {
 				clearTimeout(h);
+				setShouldKeepDynamic(false);
 			};
 		}
 	}, [lineTransform]);
@@ -298,7 +421,10 @@ export const LyricLineView: React.FC<
 				{dynamic &&
 				line.dynamicLyric &&
 				line.dynamicLyricTime &&
-				(selected || shouldKeepDynamic || forceDynamic || Math.abs(offset) < 20) ? (
+				(selected ||
+					shouldKeepDynamic ||
+					forceDynamic ||
+					Math.abs(offset) < 20) ? (
 					<div className="am-lyric-line-dynamic">
 						{line.dynamicLyric.map((word, i) => (
 							<LyricWord
