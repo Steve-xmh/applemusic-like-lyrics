d1551e38f1c33c82f8ea39cebfa677bd4cf09930
Steve-xmh <stevexmh@qq.com>
2023-12-07 09:24:51 +0800

    bncm: 根治了 macOS 的函数搜索

--- packages/bncm/src/utils/func.ts
+++ packages/bncm/src/utils/func.ts
@@ -2,14 +2,19 @@ let cachedFunctionMap: Map<string, Function> = new Map();
 
 // rome-ignore lint/suspicious/noExplicitAny: 函数类型可随意
 export function callCachedSearchFunction<F extends (...args: any[]) => any,>(
-	searchFunctionName: string | ((func: Function) => boolean),
+	searchFunctionName:
+		| string
+		| ((func: Function, funcPath: string[]) => boolean),
 	args: Parameters<F>,
+	// rome-ignore lint/suspicious/noExplicitAny: 根对象可以是任意的
+	root: any = window,
+	currentPath = ["window"],
 ): ReturnType<F> {
 	cachedFunctionMap ??= new Map(); // 很神奇，不知道为什么此处会炸
 	if (!cachedFunctionMap.has(searchFunctionName.toString())) {
-		const findResult = betterncm.ncm.findApiFunction(searchFunctionName);
-		if (findResult) {
-			const [func, funcRoot] = findResult;
+		const findResult = searchApiFunction(searchFunctionName, root, currentPath);
+		if (findResult[0]) {
+			const [func, funcRoot] = findResult[0];
 			cachedFunctionMap.set(searchFunctionName.toString(), func.bind(funcRoot));
 		}
 	}
@@ -19,4 +24,78 @@ export function callCachedSearchFunction<F extends (...args: any[]) => any,>(
 	} else {
 		throw new TypeError(`函数 ${searchFunctionName.toString()} 未找到`);
 	}
+}
+
+// 遍历对象的键，包含原型链上的键
+function* getKeys(obj: any) {
+	for (const key in obj) {
+		yield key;
+	}
+	if ("__proto__" in obj && obj.__proto__ !== Object.prototype) {
+		yield* getKeys(obj["__proto__"]);
+	}
+}
+
+export function searchApiFunction(
+	nameOrFinder: string | ((func: Function, funcPath: string[]) => boolean),
+	// rome-ignore lint/suspicious/noExplicitAny: 根对象可以是任意的
+	root: any = window,
+	currentPath = ["window"],
+	// rome-ignore lint/suspicious/noExplicitAny: 已检索对象可以是任意的
+	prevObjects: any[] = [],
+	// rome-ignore lint/suspicious/noExplicitAny: 返回该函数的携带对象，方便做 bind 绑定
+	result: [Function, any, string[]][] = [],
+	traveledObjects: Set<any> = new Set(),
+	// rome-ignore lint/suspicious/noExplicitAny: 返回该函数的携带对象，方便做 bind 绑定
+): [Function, any, string[]][] {
+	if (root === undefined || root === null) {
+		return result;
+	}
+	prevObjects.push(root);
+	if (typeof nameOrFinder === "string") {
+		if (typeof root[nameOrFinder] === "function") {
+			result.push([root[nameOrFinder], root, [...currentPath, nameOrFinder]]);
+		}
+	} else {
+		for (const key of getKeys(root)) {
+			if (typeof root[key] === "function") {
+				if (traveledObjects.has(root[key])) continue;
+				traveledObjects.add(root[key]);
+				// console.log(`${currentPath.join(".")}.${key}`);
+				const newPath = [...currentPath, key];
+				if (nameOrFinder(root[key], newPath)) {
+					result.push([root[key], root, newPath]);
+				}
+			}
+		}
+	}
+	if (currentPath.length < 10) {
+		for (const key of getKeys(root)) {
+			if (
+				typeof root[key] === "object" &&
+				// Object.hasOwn(root, key) &&
+				!prevObjects.includes(root[key]) &&
+				!(
+					currentPath.length === 1 &&
+					prevObjects[prevObjects.length - 1] === window &&
+					key === "betterncm"
+				) // 咱们自己的函数就不需要检测了
+			) {
+				if (traveledObjects.has(root[key])) continue;
+				traveledObjects.add(root[key]);
+				currentPath.push(key);
+				searchApiFunction(
+					nameOrFinder,
+					root[key],
+					currentPath,
+					prevObjects,
+					result,
+					traveledObjects,
+				);
+				currentPath.pop();
+			}
+		}
+	}
+	prevObjects.pop();
+	return result;
 }
