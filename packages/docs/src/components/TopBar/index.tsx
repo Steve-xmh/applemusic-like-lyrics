44ada57bc07f57f532bb7289c1dd0d42b7acb542
Steve-xmh <stevexmh@qq.com>
2024-01-30 12:11:46 +0800

    docs: 修正顶栏

--- packages/docs/src/components/TopBar/index.tsx
+++ packages/docs/src/components/TopBar/index.tsx
@@ -2,25 +2,28 @@ import Link from "next/link";
 import styles from "./index.module.css";
 import { LANGUAGES } from "@/lib/lang";
 
-export const TopBar = ({ lang }: { lang: string }) => {	
+export const TopBar = ({ lang }: { lang: string }) => {
 	return (
 		<header className={styles.header}>
-			<Link href={`/${lang}`}>
-				Apple Music-like Lyrics
-			</Link>
+			<Link href={`/${lang}`}>Apple Music-like Lyrics</Link>
 			<div className={styles.divider} />
-			<Link href={`${lang}/docs/core/introduction`}>
+			<div className={styles.topbarItem}>
 				文档
 				<div className={styles.menu}>
-				<Link href={`${lang}/docs/core/introduction`}>Core 模块</Link>
+					<Link href={`/${lang}/docs/core/introduction`}>Core 模块</Link>
 				</div>
-			</Link>
-			<Link href={`${lang}/docs/core/introduction`}>
-				语言<div className={styles.menu}>
-					{LANGUAGES.map(lang => (<Link href={`/${lang.lang}`}>{lang.display}</Link>))}
+			</div>
+			<div className={styles.topbarItem}>
+				语言
+				<div className={styles.menu}>
+					{LANGUAGES.map((lang, i) => (
+						<Link key={`lang-link-${i}`} href={`/${lang.lang}`}>
+							{lang.display}
+						</Link>
+					))}
 				</div>
-			</Link>
-	
+			</div>
+
 			<Link href="https://github.com/Steve-xmh/applemusic-like-lyrics">
 				Github
 			</Link>
